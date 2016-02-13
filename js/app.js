71becec8fd959889a3732137b8f6ad94b5e11574
Paige <paige.catherine.willey@gmail.com>
2016-02-13 10:02:42 -0700

    Second commit, optimized and added

--- js/app.js
+++ js/app.js
@@ -1,35 +1,43 @@
 $(document).ready(function(){
 
-$(".tweet-compose").on('click', function(){
-	$(this).css('height', '5em');
-	$('#tweet-controls').css("display", "inline-block");
-	$('.button').css('disply', 'inline-block');
-});
 
-
-$('.tweet-compose').blur(function(){
-	$('.tweet-compose').css('height', '2.5em')
-	$('#tweet-controls').css('display', 'none');
-	$('#tweet-controls').css('display','none');
-	$('#')
+$('.tweet-compose').focus('click', function(){
+	$(this).addClass('tweet-write');
+	$(this).parent().children().css('display', 'inline-block');
+	$(this).parent().children().children().css('display', 'inline-block');
 })
 
+// $(".tweet-compose").on('click', function(){
+// 	$(this).css('height', '5em');
+// 	$('#tweet-controls').css("display", "inline-block");
+// 	$('.button').css('disply', 'inline-block');
+// });
+
+
+// $('.tweet-compose').blur(function(){
+// 	$('.tweet-compose').css('height', '2.5em')
+// 	$('#tweet-controls').css('display', 'none');
+// 	$('#tweet-controls').css('display','none');
+// 	$('#')
+// })
+
 $('.tweet-compose').keyup(function(){
-	// console.log($('.tweet-compose').val().length);
-	// var text = $('#char-count').text();
-	// var num = parseInt(text);
-	// --num;
-	// $('#char-count').text(num);
  	var upper_limit = 140;
  	var text_length = $('.tweet-compose').val().length;
  	var new_count = upper_limit - text_length;
     $('#char-count').text(new_count); 
-    if(new_count < 10) {
+    if(new_count <= 10) {
     	$('#char-count').css('color', 'red');
+    	if(new_count < 0) {
+    		$('#tweet-submit').attr('disabled', 'true');
+    	}
     }
+
     else if(new_count > 10) {
     	$('#char-count').css('color', 'gray');
     }
+
 })
 
 })
+
